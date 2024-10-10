3b2c58a6b8cc9bbba890745e5d469b85661364e1
Venumurala91 <muralavenu24@gmail.com>
2024-10-10 10:28:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Max distance between same elements/max-distance-between-same-elements.py
+++ Difficulty: Easy/Max distance between same elements/max-distance-between-same-elements.py
@@ -1,28 +1,17 @@
 class Solution:
-    # Your task is to Complete this function
-    # functtion should return an integer
+
     def maxDistance(self, arr):
-        
-                
-                
-        c={}
-        m=0
-        for i in range(len(arr)):
-            c[arr[i]]=i
-            m=max(c[arr[i]],m)
-        
-        ma=0
-        for i in range(len(arr)):
-            ma=max(abs(c[arr[i]])-i,ma)
-        
-        return ma
-            
-                
-            
-        
-        
-        
-        
+        n = len(arr)
+        mp = {}
+        max_dist = 0
+        for i in range(n):
+            if arr[i] not in mp:
+                mp[arr[i]] = i
+            else:
+                max_dist = max(max_dist, i - mp[arr[i]])
+        return max_dist
+
+
 
 #{ 
  # Driver Code Starts
