724138b0a8466bda2b1d70cccf1c5cfa23139adb
Venumurala91 <muralavenu24@gmail.com>
2024-12-05 12:45:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.py
+++ Difficulty: Easy/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.py
@@ -1,15 +1,19 @@
+#{ 
+ # Driver Code Starts
+
+# } Driver Code Ends
 class Solution:
     # Function to sort an array of 0s, 1s, and 2s
     def sort012(self, arr):
+        # code here
         
+        c=arr.sort()
         
-        arr.sort()
+        return c
         
-        return arr
-
 
 #{ 
- # Driver Code Starts
+ # Driver Code Starts.
 def main():
     t = int(input().strip())  # Read the number of test cases
     ob = Solution()
@@ -22,8 +26,8 @@ def main():
         ob.sort012(arr)  # Sort the array
 
         print(' '.join(map(str, arr)))  # Print the sorted array
-
-
+        print("~")
+        
 if __name__ == "__main__":
     main()
 
