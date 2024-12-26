c64e17c098a6bf5217bef6317b44c6cdff2bed3f
Venumurala91 <muralavenu24@gmail.com>
2024-12-26 10:17:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.py
+++ Difficulty: Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.py
@@ -1,29 +1,27 @@
 #User function Template for python3
 class Solution:
-	def twoSum(self,arr, target):
+	def twoSum(self, arr, target):
 		# code here
-		
-		s = set()
+		arr.sort()
 
-    # Iterate through each element in the array
-        for num in arr:
-          
-            # Calculate the complement that added to
-            # num, equals the target
-            complement = target - num
+        left, right = 0, len(arr) - 1
     
-            # Check if the complement exists in the set
-            if complement in s:
+        # Iterate while left pointer is less than right
+        while left < right:
+            sum = arr[left] + arr[right]
+    
+            # Check if the sum matches the target
+            if sum == target:
                 return True
-    
-            # Add the current element to the set
-            s.add(num)
+            elif sum < target: 
+                left += 1  # Move left pointer to the right
+            else:
+                right -= 1 # Move right pointer to the left
     
         # If no pair is found
         return False
-		    
-		    
-		    
+
+
 
 #{ 
  # Driver Code Starts
