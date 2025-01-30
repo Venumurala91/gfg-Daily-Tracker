68cd503027b0441fe6326c11b459a5613bc2097f
Venu <muralavenu24@gmail.com>
2025-01-30 18:08:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Remove Duplicates from a Sorted Linked List/remove-duplicates-from-a-sorted-linked-list.py
+++ Difficulty: Easy/Remove Duplicates from a Sorted Linked List/remove-duplicates-from-a-sorted-linked-list.py
@@ -1,18 +1,18 @@
 #User function Template for python3
 '''
-	Your task is to remove duplicates from given 
-	sorted linked list.
-	
-	Function Arguments: head (head of the given linked list) 
-	Return Type: none, just remove the duplicates from the list.
+    Your task is to remove duplicates from given 
+    sorted linked list.
+    
+    Function Arguments: head (head of the given linked list) 
+    Return Type: none, just remove the duplicates from the list.
 
-	{
-		# Node Class
-		class Node:
-		    def __init__(self, data):   # data -> value stored in node
-		        self.data = data
-		        self.next = None
-	}
+    {
+        # Node Class
+        class Node:
+            def __init__(self, data):   # data -> value stored in node
+                self.data = data
+                self.next = None
+    }
 '''
 #Function to remove duplicates from sorted linked list.
 def removeDuplicates(head):
@@ -31,6 +31,9 @@ def removeDuplicates(head):
     
     return head
     
+
+
+
 
 #{ 
  # Driver Code Starts
